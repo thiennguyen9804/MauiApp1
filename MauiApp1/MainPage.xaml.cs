f9c319038609b96605838a9d6aba97c69da6f7fb
Hayashing only simp Kiana <thiennguyen9804@gmail.com>
2025-07-12 16:20:00 +0700

    first

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.ObjectModel;
+using MauiApp1.Models;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<TodoItem> TodoItems { get; set; } = new();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        
+    }
+
+
+    private void Button_OnClicked(object? sender, EventArgs e)
+    {
+        var todoTitle = TodoEntry.Text;
+        if (string.IsNullOrWhiteSpace(todoTitle)) return;
+        TodoItems.Add(new TodoItem { Title = todoTitle, IsCompleted = false});
+        TodoEntry.Text = "";
+
+    }
+}
---
+++ MauiApp1/Models/TodoItem.cs
@@ -0,0 +1,7 @@
+namespace MauiApp1.Models;
+
+public class TodoItem
+{
+    public required string Title { get; set; }
+    public bool IsCompleted { get; set; }
+}
